3ebbef67b021a95f26a5e834b9555b4b7fa8439f
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-18 22:44:33 +0800

    Create MainActivity.kt

---
+++ IoTAssignment/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database1 = FirebaseDatabase.getInstance("https://bait2123-202101-12-default-rtdb.firebaseio.com/")
+        val data1 = database1.getReference("PI_12_CONTROL")
+
+        data1.child("relay2").setValue("1")
+
+    }
+}
