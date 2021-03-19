64d3566bb881cadfd2285eb5c2f9e9b3a4f98f12
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-19 19:45:07 +0800

    Update MainActivity.kt

--- IoTAssignment/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ IoTAssignment/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,9 +12,14 @@ class MainActivity : AppCompatActivity() {
         val database1 = FirebaseDatabase.getInstance("https://bait2123-202101-12-default-rtdb.firebaseio.com/")
         val data1 = database1.getReference("PI_12_CONTROL")
 
-        data1.child("relay2").setValue("1")
-        data1.child("relay1").setValue("1")
+        data1.child("lcdtxt").setValue("Lim Ming Jun!!!!") // Must 16 Characters
         data1.child("camera").setValue("1")
-
+        data1.child("lcdbkR").setValue("149")
+        data1.child("lcdbkG").setValue("37")
+        data1.child("lcdbkB").setValue("12")
+        data1.child("relay1").setValue("1")
+        data1.child("relay2").setValue("0")
+        data1.child("oledsc").setValue("1")
+        data1.child("buzzer").setValue("1")
     }
 }
