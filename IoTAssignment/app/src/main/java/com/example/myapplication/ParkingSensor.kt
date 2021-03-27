8e8dfe3cd58d7ad77ee9cafe7391990333ae5adb
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2021-03-27 22:43:33 +0800

    LCC update

--- IoTAssignment/app/src/main/java/com/example/myapplication/ParkingSensor.kt
+++ IoTAssignment/app/src/main/java/com/example/myapplication/ParkingSensor.kt
@@ -3,9 +3,19 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ParkingSensor : AppCompatActivity() {
+    lateinit var parkdata:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking_sensor)
@@ -15,5 +25,39 @@ class ParkingSensor : AppCompatActivity() {
             val intent = Intent(this, SmartParking::class.java)
             startActivity(intent)
         }
+
+        val currentDateTime = LocalDateTime.now()
+
+        val formatterDate = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val formattedDate = currentDateTime.format(formatterDate)
+
+        var hour = currentDateTime.hour+8
+
+        val database1 = FirebaseDatabase.getInstance("https://bait2123-202101-12-default-rtdb.firebaseio.com/")
+                .reference.child("PI_12_$formattedDate")
+
+        parkdata=findViewById(R.id.parkingData)
+
+        var lastQuery = database1.child("$hour").orderByKey().limitToLast(1)
+        val postListener = object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for (snapshot in snapshot.children ){
+                    var sound = snapshot.child("sound").getValue().toString()
+                    Log.i("firebase", snapshot.getValue().toString())
+
+                    Log.i("sound", sound.toString())
+                    parkdata.setText(sound)
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        lastQuery.addValueEventListener(postListener)
+
     }
 }
