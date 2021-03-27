b51cc9be0b3bfd5c3742cfce2bdd8ee2a389c1b8
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-28 03:58:46 +0800

    Update LightProximity.kt

--- IoTAssignment/app/src/main/java/com/example/myapplication/LightProximity.kt
+++ IoTAssignment/app/src/main/java/com/example/myapplication/LightProximity.kt
@@ -11,13 +11,17 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.database.FirebaseDatabase
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class LightProximity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private var brightness: Sensor? = null
-    private lateinit var text: TextView
+    private lateinit var defaultBrightness: TextView
     private lateinit var pb: CircularProgressBar
+
+    private val database1 = FirebaseDatabase.getInstance("https://bait2123-202101-12-default-rtdb.firebaseio.com/")
+    val data1 = database1.getReference("PI_12_CONTROL")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,36 +34,57 @@ class LightProximity : AppCompatActivity(), SensorEventListener {
         }
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        text = findViewById(R.id.tv_text)
+        defaultBrightness = findViewById(R.id.tv_text)
         pb = findViewById(R.id.circularProgressBar)
+
+        data1.child("camera").setValue("1")
+        data1.child("oledsc").setValue("1")
+        data1.child("lcdbkR").setValue("255")
+        data1.child("lcdbkG").setValue("255")
+        data1.child("lcdbkB").setValue("0")
+        data1.child("lcdtxt").setValue("Lim Ming Jun hii")
+        data1.child("camera").setValue("1")
 
         setUpSensorStuff()
     }
 
     private fun setUpSensorStuff() {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        //brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+        if (event?.sensor?.type == Sensor.TYPE_PROXIMITY) {
             val light1 = event.values[0]
 
-            text.text = "Sensor: $light1\n${brightness(light1)}"
+            defaultBrightness.text = "Distance (m): $light1\n${brightness(light1)}"
             pb.setProgressWithAnimation(light1)
         }
     }
 
     private fun brightness(brightness: Float): String {
-
-        return when (brightness.toInt()) {
-            0 -> "Pitch black"
-            in 1..10 -> "Dark"
-            in 11..50 -> "Grey"
-            in 51..5000 -> "Normal"
-            in 5001..25000 -> "Incredibly bright"
-            else -> "This light will blind you"
+        var bright : String? = null
+        bright = when (brightness.toInt()){
+            in 5..10 -> {
+                pushBrightnessData()
+                "Brightness: 5000 lumens"
+            }
+            else -> {
+                "Brightness: 4000 lumens"
+            }
         }
+        return bright;
+    }
+
+    private fun pushBrightnessData() {
+        data1.child("oledsc").setValue("1")
+        data1.child("lcdbkR").setValue("230")
+        data1.child("lcdbkG").setValue("230")
+        data1.child("lcdbkB").setValue("0")
+        data1.child("lcdtxt").setValue("1234567891234567")
+        data1.child("camera").setValue("1")
+        //data1.child("lcdtxt").setValue("Distance: " + defaultBrightness.text.substring(14, defaultBrightness.text.indexOf("\n")) + "m")
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
