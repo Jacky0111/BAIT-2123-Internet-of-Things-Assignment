8d09575c6b1f2683cc4491de8f4e021adfe19d66
leongyitwee <leongyw-wm18@student.tarc.edu.my>
2021-03-28 14:50:47 +0800

    Update SmartDoor.kt

--- IoTAssignment/app/src/main/java/com/example/myapplication/SmartDoor.kt
+++ IoTAssignment/app/src/main/java/com/example/myapplication/SmartDoor.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class SmartDoor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,16 @@ class SmartDoor : AppCompatActivity() {
         backBtn.setOnClickListener{
             val intent = Intent(this, SmartBuilding::class.java)
             startActivity(intent)
+        }
+
+        val lockBtn = findViewById<ImageButton>(R.id.doorLock)
+        lockBtn.setOnClickListener{
+
+        }
+
+        val unlockBtn = findViewById<ImageButton>(R.id.doorUnlock)
+        unlockBtn.setOnClickListener{
+
         }
     }
 }
