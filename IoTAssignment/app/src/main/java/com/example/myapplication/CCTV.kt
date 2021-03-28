70f8d8ca241a1034cf2b95f22248e6f4ade0fb9e
leongyitwee <leongyw-wm18@student.tarc.edu.my>
2021-03-28 22:12:56 +0800

    Update CCTV.kt

--- IoTAssignment/app/src/main/java/com/example/myapplication/CCTV.kt
+++ IoTAssignment/app/src/main/java/com/example/myapplication/CCTV.kt
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import com.google.firebase.database.FirebaseDatabase
 
 class CCTV : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cctv)
+
+        val database1 = FirebaseDatabase.getInstance("https://bait2123-202101-12-default-rtdb.firebaseio.com/")
+        val data1 = database1.getReference("PI_12_CONTROL")
 
         val backBtn = findViewById<ImageButton>(R.id.backButton)
         backBtn.setOnClickListener{
