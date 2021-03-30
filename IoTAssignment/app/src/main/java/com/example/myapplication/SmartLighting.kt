3b479479c68ef8eae5325052d125be8bcc9f4138
leongyitwee <leongyw-wm18@student.tarc.edu.my>
2021-03-30 12:12:34 +0800

    Update SmartLighting.kt

--- IoTAssignment/app/src/main/java/com/example/myapplication/SmartLighting.kt
+++ IoTAssignment/app/src/main/java/com/example/myapplication/SmartLighting.kt
@@ -10,7 +10,7 @@ import com.google.firebase.database.FirebaseDatabase
 class SmartLighting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cctv)
+        setContentView(R.layout.activity_smart_lighting)
 
         val database1 = FirebaseDatabase.getInstance("https://bait2123-202101-12-default-rtdb.firebaseio.com/")
         val data1 = database1.getReference("PI_12_CONTROL")
